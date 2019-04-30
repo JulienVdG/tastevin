36920b2c012a9c712e328a765f8175a724ade858
Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>
2019-04-30 09:42:53 +0200

    serial: ensure Close end expect session
    * timeout need to be set or Read will never end when port is closed
    * Read need to manage EOF due to timeout
    * Close will close port and expect session
    
    Signed-off-by: Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>

--- pkg/serial/expect.go
+++ pkg/serial/expect.go
@@ -16,29 +16,32 @@ import (
 func (s *Serial) Spawn(timeout time.Duration, opts ...exp.Option) (*exp.GExpect, <-chan error, error) {
 	var err error
 
-	err = s.Open()
-	if err != nil {
-		return nil, nil, fmt.Errorf("error opening serial port: %v", err)
+	if s.p == nil {
+		return nil, nil, fmt.Errorf("error open serial port before calling Spawn")
 	}
 
 	resCh := make(chan error)
 
 	e, errchan, err := exp.SpawnGeneric(&exp.GenOptions{
-		In:  s.p,
-		Out: s.p,
+		In:  s,
+		Out: s,
 		Wait: func() error {
 			return <-resCh
 		},
 		Close: func() error {
 			close(resCh)
-			return s.Close()
+			err := s.p.Close()
+			s.p = nil
+			return err
 		},
 		Check: func() bool { return true },
 	}, timeout, opts...)
 	if err != nil {
-		s.Close()
+		s.p.Close()
 		return nil, nil, fmt.Errorf("error spawning serial port: %v", err)
 	}
+	s.expect = e
+	s.expectCh = errchan
 
 	return e, errchan, err
 }
--- pkg/serial/serial.go
+++ pkg/serial/serial.go
@@ -7,6 +7,11 @@
 package serial
 
 import (
+	"fmt"
+	"io"
+	"time"
+
+	exp "github.com/google/goexpect"
 	serialport "github.com/tarm/serial"
 )
 
@@ -15,8 +20,10 @@ type Config serialport.Config
 
 // Serial represent a serial port session
 type Serial struct {
-	c Config
-	p *serialport.Port
+	c        Config
+	p        *serialport.Port
+	expect   *exp.GExpect
+	expectCh <-chan error
 }
 
 // NewSerial creates a new serial port session
@@ -28,12 +35,48 @@ func NewSerial(c *Config) (*Serial, error) {
 func (s *Serial) Open() error {
 	var err error
 	c := serialport.Config(s.c)
+	if c.ReadTimeout == 0 {
+		c.ReadTimeout = time.Nanosecond
+	}
 	s.p, err = serialport.OpenPort(&c)
 	return err
 }
 
 // Close the serial session
 func (s *Serial) Close() error {
-	return s.p.Close()
+	if s.p == nil {
+		return nil
+	}
+	if s.expect != nil {
+		err := s.expect.Close()
+		if err != nil {
+			return fmt.Errorf("error closing expect: %v", err)
+		}
+		// Ensure the logs are closed by waiting the complete end
+		<-s.expectCh
+		// expect will close the serial port, exit now
+		return nil
+	}
+	err := s.p.Close()
+	s.p = nil
+	return err
+}
 
+func (s *Serial) Read(b []byte) (n int, err error) {
+	if s.p == nil {
+		return 0, fmt.Errorf("read %s port closed", s.c.Name)
+	}
+	n, err = s.p.Read(b)
+	// Handle read timeout, should not return EOF until closed
+	if n == 0 && err == io.EOF && s.p != nil {
+		err = nil
+	}
+	return
+}
+
+func (s *Serial) Write(b []byte) (n int, err error) {
+	if s.p == nil {
+		return 0, fmt.Errorf("write %s port closed", s.c.Name)
+	}
+	return s.p.Write(b)
 }

44e4d38600c1c6b7530de615773e740479355b1c
Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>
2019-05-15 15:13:31 +0200

    json2test: basic infrastructure for parsing test2json output
    Signed-off-by: Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>

---
+++ pkg/json2test/verbose.go
@@ -0,0 +1,29 @@
+// Copyright 2019 Splitted-Desktop Systems. All rights reserved
+// Copyright 2019 Julien Viard de Galbert
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package json2test
+
+import (
+	"io"
+	"log"
+)
+
+type verboseHandler struct {
+	w io.Writer // stdout for testing output stream
+}
+
+// NewVerboseHandler return a TestEventHandler that writes 'output' events to w.
+func NewVerboseHandler(w io.Writer) TestEventHandler {
+	return verboseHandler{w: w}
+}
+
+func (v verboseHandler) Handle(e TestEvent) {
+	if e.Action == "output" {
+		_, err := v.w.Write([]byte(e.Output))
+		if err != nil {
+			log.Printf("json2test verbose output: %s", err)
+		}
+	}
+}

c6abad44ba468d56467d941862a273670d71f9ef
Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>
2019-08-30 15:45:22 +0200

    pkg/gotestweb: update jquery to 3.4.1
    Signed-off-by: Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>

--- pkg/gotestweb/template.go
+++ pkg/gotestweb/template.go
@@ -70,7 +70,7 @@ var indexTemplate = `<!DOCTYPE html>
     </div>
 
 {{if .UseCDN}}
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 {{else}}
     <script src="{{.AppPrefix}}vendor/jquery/jquery.min.js"></script>
